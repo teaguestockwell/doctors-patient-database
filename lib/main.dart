429ef70ce2766afc2fb79d670b692050a8a04a41
teagueStockwell <tsAppDevelopment@gmail.com>
2021-01-28 09:16:16 -0800

    auth

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:doctors_patient_database/screens/auth_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,7 @@ void main() async {
     MaterialApp(
       title: 'Flutter Demo',
       theme: theme,
-      home: ProfileSearch()
+      home: Auth()
       //home: LastCheckup(id:'234567890')
     )
   );
---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,86 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../constant.dart';
+import '../widgets/but.dart';
+import 'profile_search.dart';
+
+
+
+
+class Auth extends StatefulWidget {
+  @override
+  _AuthState createState() => _AuthState();
+}
+
+class _AuthState extends State<Auth> {
+  String email = '';
+  String pass = '';
+  Widget signInPage;
+
+  @override
+  void initState(){
+    super.initState();
+    
+    signInPage =  Scaffold(body:Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Center(child: ListView(children:[
+        TextField(
+          decoration: InputDecoration(labelText: 'email'),
+          onChanged: (x) => email = x,
+        ),
+
+        TextField(
+          decoration: InputDecoration(labelText: 'password'),
+          onChanged: (x) => pass = x
+        ),
+      
+        But(text:'login',
+        onpress: login,
+        ),
+
+      ])),
+    ));
+  }
+
+  void login() async {
+    try{
+      if(email == ''){email = ' ';}
+      if(pass == ''){pass = ' ';}
+      await FirebaseAuth.instance
+      .signInWithEmailAndPassword(email: email, password: pass).timeout(Duration(seconds: 10));
+    // ignore: avoid_catches_without_on_clauses
+    }catch(_){showSnack();}
+  }
+
+  void showSnack(){
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Container(
+      height: Snackz.y, 
+      child: Center(
+        child: Text('Login Error',style: Snackz.ts,
+  )))));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User>(
+      stream: FirebaseAuth.instance.onAuthStateChanged,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.active) {
+          User user = snapshot.data;
+          if (user == null) {
+            return signInPage;
+          }
+          return ProfileSearch();
+        } else {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      },
+    );
+  }
+}
